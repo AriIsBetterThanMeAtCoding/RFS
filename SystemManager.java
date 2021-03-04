d5478fc641fb1d64fe0e300b6a63ee7bda2975dd
AriIsBetterThanMeAtCoding <80062778+AriIsBetterThanMeAtCoding@users.noreply.github.com>
2021-03-04 10:01:04 -0800

    Add files via upload

---
+++ SystemManager.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface SystemManager {
+    ArrayList<Location> locations = new ArrayList<Location>();
+    ArrayList<Leg> legs = new ArrayList<Leg>();
+    
+    
+}
