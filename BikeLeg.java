68b52ba3a12dbdc3b5a164337bd220333dea1ec8
Robert <roberth9368@sd61learn.ca>
2021-03-04 10:18:38 -0800

    BikeLeg Constructor

--- BikeLeg.java
+++ BikeLeg.java
@@ -1,4 +1,12 @@
 
 public class BikeLeg {
-
+    Location o = new Location();
+    Location d = new Location();
+    int distance
+    
+    
+    
+    Public BikeLeg(Location origin, Locaiton destination, int distanceInKm, String days) {
+        
+    }
 }
