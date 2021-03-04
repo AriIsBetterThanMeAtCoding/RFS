cbd3bd8cda51d58ebee31454a9597ada5e1c1fbc
Robert <roberth9368@sd61learn.ca>
2021-03-04 10:43:55 -0800

    IM not ready for htis

--- Leg.java
+++ Leg.java
@@ -1,4 +1,10 @@
 
-public class Leg {
-
+public abstract class Leg {
+    Location origin = new Location();
+    Location destination = new Location();
+    String daysAvailable = "";
+    int distance =0;
+    double costPerKm = 0;
+    
+    
 }
