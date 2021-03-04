c4a4abd86d66554dc859891622483a29ade010e4
Robert <roberth9368@sd61learn.ca>
2021-03-04 10:24:00 -0800

    thing

--- BikeLeg.java
+++ BikeLeg.java
@@ -2,11 +2,15 @@
 public class BikeLeg {
     Location o = new Location();
     Location d = new Location();
-    int distance
+    int di=0;
+    String day = "";
     
     
     
     Public BikeLeg(Location origin, Locaiton destination, int distanceInKm, String days) {
-        
+        o=origin;
+        d=destination;
+        di = distance;
+        day=days;
     }
 }
