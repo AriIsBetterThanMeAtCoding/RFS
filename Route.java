6324b775f560e13ecd9fc9faf6359c5a8ab2eebf
Robert <roberth9368@sd61learn.ca>
2021-03-04 11:19:30 -0800

    f

--- Route.java
+++ Route.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Route {
+public class Route implements SystemManager{
     ArrayList<Leg> legs = new ArrayList<Leg>();
     
     public double totalCost() {
@@ -16,6 +16,36 @@ public class Route {
     }
     
     public void addLeg(Leg leg) {
+        
+    }
+
+    @Override
+    public void addLocation(Location l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void findLocation(String name) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void findCheapestRoute(Location origin, Location destination, String day) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void findMinStepsRoute(Location origin, Location destination, String day) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void findShortestKmRoute(Location origin, Location destination, String day) {
+        // TODO Auto-generated method stub
         
     }
 }
