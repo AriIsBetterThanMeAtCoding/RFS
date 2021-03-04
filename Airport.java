835fe089acb48c35b400cf0c7b584248183f6569
Robert <roberth9368@sd61learn.ca>
2021-03-04 10:39:49 -0800

    ARIS WORKING YAY

--- Airport.java
+++ Airport.java
@@ -1,13 +1,8 @@
 
 public class Airport {
     
-<<<<<<< HEAD
-    // vibez and that // test // test again
-    // another test // Happy
-    // happy maybe?
-=======
->>>>>>> branch 'main' of https://github.com/AriIsBetterThanMeAtCoding/RFS
+
     public Airport() {
-        System.out.println("Hi");
+
     }
 }
