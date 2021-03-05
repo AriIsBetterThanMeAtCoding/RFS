28a51d6ca1bf35539cd697d725cfea0aae80b162
robert <roberth9368@sd61learn.ca>
2021-03-05 10:36:08 -0800

    Comments

--- Leg.java
+++ Leg.java
@@ -14,26 +14,45 @@ public abstract class Leg {
         costPerKm = cost;
     } // Leg
     
+    
+    /*
+     * Postconditions: returns the Origin Location
+     */
     public Location getOrigin() {
         return origin;
     } // getOrigin
     
+    /*
+     * Postconditions: returns the Destination Location
+     */
     public Location getDestination() {
         return destination;
     } // getDestination
     
+    /*
+     * Postconditions: returns the Days Available
+     */
     public String getDaysAvailable() {
         return daysAvailable;
     } // getDaysAvailable
     
+    /*
+     * Postconditions: returns the Distance
+     */
     public int getDistance() {
         return distance;
     } // getDistance
     
+    /*
+     * Postconditions: returns the getCostPerKm
+     */
     public double getCostPerKm() {
         return costPerKm;
     } // getCostPerKm
     
+    /*
+     * Postconditions: returns the toString
+     */
     public String toString() {
         return "Origin: " + origin + " Destination: " + destination;
     } // toString
