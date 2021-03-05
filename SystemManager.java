44b8569fb528a28beb7cde16d24428e985ac6b1e
robert <roberth9368@sd61learn.ca>
2021-03-05 09:26:45 -0800

    f

--- SystemManager.java
+++ SystemManager.java
@@ -4,12 +4,24 @@ public class SystemManager {
     ArrayList<Location> locations = new ArrayList<Location>();
     ArrayList<Leg> legs = new ArrayList<Leg>();
     
-    public void addLocation(Location l);
-    public void addLeg(Leg l);
-    public void findLocation(String name);
-    public void findCheapestRoute (Location origin, Location destination, String day);
-    public void findMinStepsRoute (Location origin, Location destination, String day);
-    public void findShortestKmRoute (Location origin, Location destination, String day);
+    public void addLocation(Location l) {
+        
+    }
+    public void addLeg(Leg l) {
+        
+    }
+    public void findLocation(String name) {
+        
+    }
+    public void findCheapestRoute (Location origin, Location destination, String day) {
+        
+    }
+    public void findMinStepsRoute (Location origin, Location destination, String day) {
+        
+    }
+    public void findShortestKmRoute (Location origin, Location destination, String day) {
+        
+    }
     
     
 }
