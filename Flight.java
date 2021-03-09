4ba9c7a1de3a4d5d22d50a5a9ea5034fdf26fb8a
Robert <roberth9368@sd61learn.ca>
2021-03-09 10:23:32 -0800

    FLYING TRUCKLEGS!!

--- Flight.java
+++ Flight.java
@@ -3,6 +3,13 @@ public class Flight extends Leg {
 
     public Flight(Location origin, Location destination, int distanceInKm, String days) {
         super(origin, destination, distanceInKm, days, 0.23);
+
+        boolean isValid = origin instanceof Airport && destination instanceof Airport;
+
+        if (!isValid) {
+            System.out.println("Leg did not include the correct destination or origin");
+            System.exit(0);
+        }
     } // Flight
 
 } // Flight
--- TruckLeg.java
+++ TruckLeg.java
@@ -1,12 +1,13 @@
 
 public class TruckLeg extends Leg {
-    
+
     // Calls super constructor. Fixed cost/km is $0.58
     public TruckLeg(Location origin, Location destination, int distanceInKm, String days) {
         super(origin, destination, distanceInKm, days, 0.58);
-        boolean isValid = (origin instanceof TruckDepot || origin instanceof Airport) && (destination instanceof TruckDepot || destination instanceof Airport);
-        
-        if(!isValid) {
+        boolean isValid = (origin instanceof TruckDepot || origin instanceof Airport)
+                && (destination instanceof TruckDepot || destination instanceof Airport);
+
+        if (!isValid) {
             System.out.println("Leg did not include the correct destination or origin");
             System.exit(0);
         }
