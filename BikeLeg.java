503a5fd13d6005ac9635e92b00460265fb1b8cac
AriveIT <ari15901590@gmail.com>
2021-03-09 10:26:23 -0800

    error checking for bikeleg and leg

--- BikeLeg.java
+++ BikeLeg.java
@@ -1,6 +1,16 @@
 
 public class BikeLeg extends Leg {
     public BikeLeg(Location origin, Location destination, int distanceInKm, String days) {
+        
         super(origin, destination, distanceInKm, days, 1);
+        
+        // check if leg is valid
+        if(!((origin instanceof BikeDeliveryLocation && (destination instanceof BikeDeliveryLocation 
+                || destination instanceof TruckDepot) || origin instanceof TruckDepot 
+                && origin instanceof BikeDeliveryLocation))) {
+            System.out.println("Bike leg origin and destinations must be a TruckDepot and BikeDeliveryStation or"
+                    + " 2 BikeDeliveryStations");
+            System.exit(0);
+        }
     } // BikeLeg
 } // BikeLeg
