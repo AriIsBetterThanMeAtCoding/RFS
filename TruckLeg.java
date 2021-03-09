65c7259a918b29e9f67dfc9fe2d125db5913df83
Robert <roberth9368@sd61learn.ca>
2021-03-09 10:53:51 -0800

    what what what I ve got a

--- TruckLeg.java
+++ TruckLeg.java
@@ -4,8 +4,8 @@ public class TruckLeg extends Leg {
     // Calls super constructor. Fixed cost/km is $0.58
     public TruckLeg(Location origin, Location destination, int distanceInKm, String days) {
         super(origin, destination, distanceInKm, days, 0.58);
-        boolean isValid = (origin instanceof TruckDepot || origin instanceof Airport)
-                && (destination instanceof TruckDepot || destination instanceof Airport);
+        boolean isValid = (origin instanceof TruckDepot && destination instanceof Airport)
+                || (origin instanceof Airport && destination instanceof TruckDepot);
 
         if (!isValid) {
             System.out.println("Leg did not include the correct destination or origin");
